a160282d8b060de2a56e5582fd5cf59254e0ae8a
liyangzhen <liyangzhen@qjdchina.com>
2022-12-07 16:30:02 +0800

    json序列化优化

--- lyz-common/common-controller/src/main/java/com/lyz/security/common/controller/config/CommonControllerAutoConfig.java
+++ lyz-common/common-controller/src/main/java/com/lyz/security/common/controller/config/CommonControllerAutoConfig.java
@@ -1,11 +1,21 @@
 package com.lyz.security.common.controller.config;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * 注释:
@@ -28,5 +38,24 @@ public class CommonControllerAutoConfig extends WebMvcConfigurationSupport {
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**").addResourceLocations("classpath:/META-INF/resources/");
         super.addResourceHandlers(registry);
+    }
+
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        super.extendMessageConverters(converters);
+        if (!CollectionUtils.isEmpty(converters)) {
+            Optional<HttpMessageConverter<?>> optional = converters.stream().filter(item -> item instanceof MappingJackson2HttpMessageConverter).findFirst();
+            optional.ifPresent(item -> {
+                MappingJackson2HttpMessageConverter converter = (MappingJackson2HttpMessageConverter) item;
+                ObjectMapper objectMapper = converter.getObjectMapper();
+                //生成JSON时,将所有Long转换成String
+                SimpleModule simpleModule = new SimpleModule();
+                simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+                simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+                objectMapper.registerModule(simpleModule);
+                //BigDecimal转化为PlainToString
+                objectMapper.enable(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN);
+            });
+        }
     }
 }
